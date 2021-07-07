ada2e394c20b25285c8af7a00be889c21e7463e2
FrankQiu <iam.frankqiu@gmail.com>
2021-07-07 17:27:03 +0800

    feat: add prettier-config

---
+++ packages/prettier-config/README.md
@@ -0,0 +1,39 @@
+
+# @base/prettier-config
+
+My shareable prettier config.
+
+
+## Installation
+
+Install  with npm
+
+```bash
+  npm install --save-dev @base/prettier-config
+```
+
+or with yarn
+
+```bash
+  yarn add --dev @base/prettier-config
+```
+## Usage
+
+Reference @jdp-dev/prettier-config in your package.json.
+
+
+```javascript
+{
+  "name": "my-cool-library",
+  "version": "9000.0.1",
+  "prettier": "@base/prettier-config"
+}
+
+```
+
+  
+## License
+
+[MIT](../../LICENSE)
+
+  
---
+++ packages/prettier-config/index.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "arrowParens": "always",
+  "bracketSpacing": true,
+  "embeddedLanguageFormatting": "auto",
+  "htmlWhitespaceSensitivity": "css",
+  "insertPragma": true,
+  "jsxBracketSameLine": false,
+  "jsxSingleQuote": true,
+  "printWidth": 100,
+  "proseWrap": "preserve",
+  "quoteProps": "as-needed",
+  "requirePragma": false,
+  "semi": false,
+  "singleQuote": true,
+  "tabWidth": 2,
+  "trailingComma": "es5",
+  "useTabs": false,
+  "vueIndentScriptAndStyle": false
+}
